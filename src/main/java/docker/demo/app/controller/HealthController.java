43d2fac496d0c9b0f908ebc2b6bb4caaf0b89823
nikne <nesterenklo-nikita@bk.ru>
2023-01-28 17:07:46 +0300

    Kubernetes

--- src/main/java/docker/demo/app/controller/HealthController.java
+++ src/main/java/docker/demo/app/controller/HealthController.java
@@ -1,14 +1,17 @@
 package docker.demo.app.controller;
 
 import docker.demo.app.dto.rs.BasicResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
     public BasicResponse health() {
+        log.info("HealthCheck process...");
         return new BasicResponse("OK");
     }
 }
